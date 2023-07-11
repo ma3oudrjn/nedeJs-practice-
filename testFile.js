720cdceabec45c24c609a1bba39492292acc68f9
masoud <masoudrng2@gmail.com>
2023-07-11 17:03:31 +0330

    little DoM

--- testFile.js
+++ testFile.js
@@ -2,6 +2,11 @@ const http = require('http');
 const server =http.createServer((req,res)=>{
 
 console.log(req.url, req.method, req.headers);
-process.exit();
+res.setHeader('Contax-Type','text/html');
+res.write('<html>');
+res.write('<head><title>may page</title></head>');
+res.write('<body><h1>hello</h1></body>');
+res.write('</html>');
+res.end();
 });
 server.listen(3000)
