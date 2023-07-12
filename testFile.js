c4b9d0ea4e6d5eed01210f15ee05b084a91dea7b
masoud <masoudrng2@gmail.com>
2023-07-12 08:45:08 +0330

    bunch of erors

--- testFile.js
+++ testFile.js
@@ -1,12 +1,22 @@
 const http = require('http');
 const server =http.createServer((req,res)=>{
+const url=req.url;
+if(req==='/'){
+    res.setHeader('Contax-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>do some shit</title></head>');
+    res.write('<body><from action="/massage" method="POST"><input type="text" "name=message">button type="submit"></from></button></body>');
+    res.write('</html>');
+    res.end();
+return res.end();
+}
 
-console.log(req.url, req.method, req.headers);
-res.setHeader('Contax-Type','text/html');
-res.write('<html>');
-res.write('<head><title>may page</title></head>');
-res.write('<body><h1>hello</h1></body>');
-res.write('</html>');
-res.end();
+
+// res.setHeader('Contax-Type','text/html');
+// res.write('<html>');
+// res.write('<head><title>may page</title></head>');
+// res.write('<body><h1>hello</h1></body>');
+// res.write('</html>');
+
 });
 server.listen(3000)
