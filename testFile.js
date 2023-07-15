56d24d480f111f508ee5a8d4da839c5a5ac5bbcc
masoud <masoudrng2@gmail.com>
2023-07-15 08:20:57 +0330

    after 2  days

--- testFile.js
+++ testFile.js
@@ -1,7 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 
 const  server = http.createServer((req, res) => {
-console.log(req.url, req.headers , req.method);
+// console.log(req.url, req.headers , req.method);
+const url= req.url;
+const method=req.method;
+if(url==='/'){
+    res.write('<html>');
+    res.write('<head><title>massasge</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text"><button type="sunmit">seend</button></body>');
+    res.write('</html>');
+    return res.end(); 
+}
+if(url === 'message' && method === 'POST'){
+fs.writeFileSync('massage.txt', 'hello');
+res.statusCode=302;
+res.setHeader('location', '/')
+return res.end(); 
+
+}
+
 res.setHeader('content-Type', 'text/html');
 res.write('<html>');
 res.write('<head><title>HI</title></head>');
