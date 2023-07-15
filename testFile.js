7050dda62357ce0483246c11a05d41ca8e4a01ef
masoud <masoudrng2@gmail.com>
2023-07-15 11:41:03 +0330

    final copy

--- testFile.js
+++ testFile.js
@@ -1,51 +1,9 @@
 const http = require('http');
-const fs = require('fs'); // File System core mudule
-const { buffer } = require('stream/consumers');
-const server =http.createServer((req ,res)=>{
-const Url=req.url;
-const Method=req.method;
 
- if (Url === '/'){
-    res.write('<html>')
-    res.write('<head><title>heloo</title></head>')
-    res.write('<body><form action="/message" method="POST"><input type="text>"><button type="submit">Send</button></input></form></body>')
-    res.write('</html>')
-    return res.end(); 
- }
-//  && Method ==='POST'
- if (Url ==='/message' ){
-    let allData=[];
-    fs.writeFileSync('message.txt', message);
-    res.statusCode = 302;
-    res.setHeader('location','/')
-    req.on('data',(chunk) =>{
-        console.log(chunk); 
- allData.push(chunk);
-    });
+const routes = require('./routes');
 
-req.on('end', () =>{
+console.log(routes.someText);
 
-const bodyTo = buffer.concat(body).toString();
-console.log(bodyTo);
-const message=bodyTo.splite("=")[1]; 
-});
+const server = http.createServer(routes.handler);
 
-
-}
- res.write('<html>')
- res.write('<head><title>heloo</title></head>')
- res.write('<body><h1>hi</h1></body>')
- res.write('</html>')
- res.end();
- 
-
-res.setHeader('Content-Type', 'text/html');
-
-
-
-
-
-});
-
-
-server.listen(8000);
+server.listen(3000);
