2f990370dc16d20939cdb8bfda4272e95c405abd
masoud <masoudrng2@gmail.com>
2023-07-15 10:35:19 +0330

    File System core mudule

--- testFile.js
+++ testFile.js
@@ -1,31 +1,51 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs'); // File System core mudule
+const { buffer } = require('stream/consumers');
+const server =http.createServer((req ,res)=>{
+const Url=req.url;
+const Method=req.method;
 
-const  server = http.createServer((req, res) => {
-// console.log(req.url, req.headers , req.method);
-const url= req.url;
-const method=req.method;
-if(url==='/'){
-    res.write('<html>');
-    res.write('<head><title>massasge</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text"><button type="sunmit">seend</button></body>');
-    res.write('</html>');
+ if (Url=== '/'){
+    res.write('<html>')
+    res.write('<head><title>heloo</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text>"><button type="submit">Send</button></input></form></body>')
+    res.write('</html>')
     return res.end(); 
-}
-if(url === 'message' && method === 'POST'){
-fs.writeFileSync('massage.txt', 'hello');
-res.statusCode=302;
-res.setHeader('location', '/')
-return res.end(); 
+ }
 
-}
+ if (Url ==='/message' && Method ==='POST'){
+    let allData=[];
+    fs.writeFileSync('message.txt','hiiiiii');
+    res.statusCode = 302;
+    res.setHeader('LOcation','/')
+    req.on('data',(chunk) =>{
+        console.log(chunk);
+ allData.push(chunk);
+    });
 
-res.setHeader('content-Type', 'text/html');
-res.write('<html>');
-res.write('<head><title>HI</title></head>');
-res.write('<body><h1>hello sir</h1></body>');
-res.write('</html>');
-res.end();
+req.on('end', () =>{
+
+const bodyTo = buffer.concat(body).toString();
+console.log(bodyTo);
+const message=bodyTo.splite("=")[1]; 
 });
+
+
+}
+ res.write('<html>')
+ res.write('<head><title>heloo</title></head>')
+ res.write('<body><h1>hi</h1></body>')
+ res.write('</html>')
+ res.end();
+ 
+
+res.setHeader('Content-Type', 'text/html');
+
+
+
+
+
+});
+
 
 server.listen(8000);
